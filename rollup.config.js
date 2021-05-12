8ad368ee53daab424ecc0534b7c14747920e3994
Natthaphat Piasangka <n.devs260340@gmail.com>
2021-05-12 19:31:27 +0700

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 
-// import commonjs from "rollup-plugin-commonjs";
+import commonjs from 'rollup-plugin-commonjs';
+import hashbang from 'rollup-plugin-hashbang';
+import dts from 'rollup-plugin-dts';
 
 export default [
     {
-      input: "lib/cli.js",
+      input: "lib/cli-index.js",
       output: {
-        file: "dist/cli.js",
+        file: "dist/cli-index.js",
         format: "cjs"
       },
       external: ["shelljs", "yargs"]
@@ -16,8 +18,26 @@ export default [
         file: "dist/index.js",
         format: "cjs"
       },
-      // plugins: [
-      //   commonjs()
-      // ]
-    }
+      plugins: [
+        hashbang(),
+        commonjs()
+      ]
+    },
+    {
+      input: "lib/cli.js",
+      output: {
+        file: "dist/cli.js",
+        format: "cjs"
+      },
+      plugins: [
+        hashbang()
+      ]
+    },
+    {
+      input: "lib/types.d.ts",
+      output: [{ file: "dist/types.d.ts", format: "es" }],
+      plugins: [dts()],
+    },
+  
+
   ];
